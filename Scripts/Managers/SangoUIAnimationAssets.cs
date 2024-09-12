8d43721a0c61ae8660617323191e52c66d46bc3a
Sangonomiya Sakunovi <1602274566@qq.com>
2024-09-12 10:04:55 +0800

    init push

---
+++ Scripts/Managers/SangoUIAnimationAssets.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SangoUtils.Tweens_Unity
+{
+    public static class SangoUIAnimationAssets
+    {
+        public static SangoUIAnimationAssets_ResSangoUISlideBroadCastImageAnimation SangoUISlideBroadCastImageAnimation(Transform parentTrans, float aniDurationTime)
+        {
+            SangoUIAnimationAssets_ResSangoUISlideBroadCastImageAnimation animation = new SangoUIAnimationAssets_ResSangoUISlideBroadCastImageAnimation(parentTrans, aniDurationTime);
+            animation.InitAnimation();
+            return animation;
+        }
+    }
+}
